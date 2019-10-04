4d8c23889a07e5d5d8e88e881acaf0c6ba07c8d1
Cédric <cedric-garcia@sfr.fr>
2019-10-04 13:18:05 +0200

    fix: go back buttons

--- src/app/sessions/session-details-page/session-details-page.component.ts
+++ src/app/sessions/session-details-page/session-details-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { SessionsStoreService } from "src/app/services/sessions-store.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-session-details-page",
@@ -13,12 +13,17 @@ export class SessionDetailsPageComponent implements OnInit {
 
   constructor(
     public sessionsStore: SessionsStoreService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.sessionId = +params.get("id");
     });
+  }
+
+  goBack(): void {
+    this.router.navigate(["/sessions"]);
   }
 }
