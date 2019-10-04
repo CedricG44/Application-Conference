712fd373be7ef4cb768844b0e86a0bf754395b1a
Cédric <cedric-garcia@sfr.fr>
2019-10-04 15:57:32 +0200

    feat: Android support

--- src/app/speakers/components/speakers-detail/speakers-detail.component.ts
+++ src/app/speakers/components/speakers-detail/speakers-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Speaker } from "../../speaker.model";
+import {
+  Contacts,
+  Contact,
+  ContactField,
+  ContactName
+} from "@ionic-native/contacts/ngx";
 
 @Component({
   selector: "app-speakers-detail",
@@ -9,7 +15,21 @@ import { Speaker } from "../../speaker.model";
 export class SpeakersDetailComponent implements OnInit {
   @Input() speaker: Speaker;
 
-  constructor() {}
+  constructor(private contacts: Contacts) {}
 
   ngOnInit() {}
+
+  addToContacts(event, speaker: Speaker) {
+    console.log(event);
+    let contact: Contact = this.contacts.create();
+
+    contact.name = new ContactName(null, "Smith", "John");
+    contact.phoneNumbers = [new ContactField("mobile", "6471234567")];
+    contact
+      .save()
+      .then(
+        () => console.log("Contact saved!", contact),
+        (error: any) => console.error("Error saving contact.", error)
+      );
+  }
 }
